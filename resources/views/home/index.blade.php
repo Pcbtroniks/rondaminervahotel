b75d0363799d877d6a2c687509b70f64a7026315
Pcbtroniks <desarrollo.software@pcbtroniks.com>
2022-07-07 13:29:25 -0500

    Opmtimized Hero Image

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -25,7 +25,7 @@
 @section('content')
     
     <!-- Hero Area Section Begin -->
-    <section class="hero-area set-bg" data-setbg="{{ asset('custom/img/hero_2.jpg')}}">
+    <section class="hero-area set-bg" data-setbg="{{ asset('custom/img/hero_2-min.jpg')}}">
         <div class="container">
             <div class="row">
                 <div class="col-lg-12 text-center">
